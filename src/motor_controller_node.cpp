33f2c6a126fbcfee2a10851cdd780bf98067d7eb
ras <ras@RAS28.(none)>
2014-10-16 17:53:46 +0200

    Resetted all k constants to 1

--- src/motor_controller_node.cpp
+++ src/motor_controller_node.cpp
@@ -30,10 +30,10 @@
 // NB might have to increase KP and decrease KI
 #define PARAM_DEFAULT_LEFT_KP           1.0
 #define PARAM_DEFAULT_RIGHT_KP          -1.0
-#define PARAM_DEFAULT_LEFT_KI           2.0
-#define PARAM_DEFAULT_RIGHT_KI          -2.0
-#define PARAM_DEFAULT_LEFT_KD           0.2
-#define PARAM_DEFAULT_RIGHT_KD          -0.2
+#define PARAM_DEFAULT_LEFT_KI           1.0
+#define PARAM_DEFAULT_RIGHT_KI          -1.0
+#define PARAM_DEFAULT_LEFT_KD           1.0
+#define PARAM_DEFAULT_RIGHT_KD          -1.0
 #define PARAM_DEFAULT_WHEEL_RADIUS      0.05
 #define PARAM_DEFAULT_ROBOT_BASE        0.225
 #define PARAM_DEFAULT_TICKS_PER_REV     360
