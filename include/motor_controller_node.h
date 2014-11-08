5b96043f64003493d32bf78cdad4183691700bad
Lucas Wiener <lucas@wiener.se>
2014-11-08 15:52:23 +0100

    Added RotationDirection enum and to_string function

--- include/motor_controller_node.h
+++ include/motor_controller_node.h
@@ -13,6 +13,30 @@ namespace s8 {
         const char *        TOPIC_ACTUAL_TWIST =        "/s8/actual_twist";
 
         const char *        ACTION_STOP =               "stop";
+
+        /**
+         * Positive angular velocity (w) will make the robot spin counter-clockwise (left).
+         * Negative angular velocity (w) will make the robot spin clockwise (right).
+         * 0 angular velocity will not make the robot spin in any direction (it will always point forward).
+         */
+        enum RotationDirection {
+            LEFT = 1,
+            FORWARD = 0,
+            RIGHT = -1
+        };
+
+        /**
+         * Converts the RotationDirection to a string describing the rotation direction.
+         */
+        std::string to_string(RotationDirection rotation_direction) {
+            switch(rotation_direction) {
+                case RotationDirection::LEFT: return "LEFT";
+                case RotationDirection::FORWARD: return "FORWARD";
+                case RotationDirection::RIGHT: return "RIGHT";
+            }
+
+            return "UNKNOWN";
+        }
     }
 }
 
